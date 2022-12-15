3d42a61902ca892905dc40e6b6bde2824ec0ec0d
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-12-14 22:22:54 -0500

    Update app js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -6,6 +6,9 @@ import { Ionicons } from "@expo/vector-icons";
 import { TutorProvider } from "./context/tutorContext";
 
 //Screens
+import HomeScreenNav from "./navigation/HomeScreenNav/HomeScreenNav";
+import SearchScreenNav from "./navigation/SearchScreenNav/SearchScreenNav";
+import ProfileScreen from "./components/ProfileScreen/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,12 +17,50 @@ export default function App() {
     <SafeAreaProvider style={styles.container}>
       <TutorProvider>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen />
-            <Tab.Screen />
-            <Tab.Screen />
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === "Dashboard") {
+                  iconName = focused ? "ios-home" : "ios-home-outline";
+                } else if (route.name === "Search") {
+                  iconName = focused ? "ios-search" : "ios-search-outline";
+                } else if (route.name === "Profile") {
+                  iconName = focused ? "person" : "person-outline";
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              headerStyle: {
+                backgroundColor: "#127369",
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontSize: 16,
+                fontWeight: "bold",
+              },
+              tabBarStyle: { backgroundColor: "#10403B" },
+              tabBarActiveTintColor: "#fff",
+              tabBarInactiveTintColor: "#bfbfbf",
+            })}
+          >
+            <Tab.Screen
+              name="Dashboard"
+              component={HomeScreenNav}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Search"
+              component={SearchScreenNav}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
           </Tab.Navigator>
         </NavigationContainer>
+        <StatusBar style="light" />
       </TutorProvider>
     </SafeAreaProvider>
   );
