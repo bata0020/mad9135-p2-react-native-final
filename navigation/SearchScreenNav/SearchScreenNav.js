ae9601f6974a6b3699c207038ee7a145eb618569
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-12-14 21:45:16 -0500

    Add search screen nav

---
+++ navigation/SearchScreenNav/SearchScreenNav.js
@@ -0,0 +1,27 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import SearchScreen from "../SearchScreen/SearchScreen";
+import TutorInfoScreen from "../TutorInfoScreen/TutorInfoScreen";
+
+const Stack = createStackNavigator();
+
+function SearchScreenNav() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#127369",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontSize: 16,
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="Search Tutors" component={SearchScreen} />
+      <Stack.Screen name="Details" component={TutorInfoScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default SearchScreenNav;
