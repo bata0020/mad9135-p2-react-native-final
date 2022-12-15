45fb84bef6f4b4d84f2c17da48d801d810fec30b
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-12-14 21:58:41 -0500

    Add favorite screen

---
+++ components/FavoritesScreen/FavoritesScreen.js
@@ -0,0 +1,58 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, StyleSheet, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { useTutor } from "../../context/tutorContext";
+import TutorItem from "../TutorItem/TutorItem";
+
+function FavoritesScreen() {
+  const [tutors] = useTutor();
+  const [favoriteTutor, setFavoriteTutor] = useState([]);
+
+  useEffect(() => {
+    setFavoriteTutor(tutors);
+  }, [favoriteTutor.favorite]);
+
+  const faves = favoriteTutor.filter((person) => person.favorite === true);
+
+  console.log(faves);
+
+  if (!tutors) {
+    return null;
+  } else if (faves.length === 0) {
+    return (
+      <SafeAreaView style={styles.empty} edges={["right", "left"]}>
+        <Text>Favorite tutors will appear here!</Text>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container} edges={["right", "left"]}>
+        <FlatList
+          data={faves}
+          renderItem={({ item }) => (
+            <TutorItem
+              tutor={item}
+              onLongPress={() => console.log((item.favorite = !item.favorite))}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e2e2e2",
+    height: "100%",
+  },
+  empty: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#e2e2e2",
+  },
+});
+
+export default FavoritesScreen;
